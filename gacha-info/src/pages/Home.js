1a3528bc112a3f403d3ccc0e10721a87342d755e
TongJetKit <jetkittong@gmail.com>
2025-03-04 14:35:51 +0800

    Update react/tailwind and added daisyUI

--- gacha-info/src/pages/Home.js
+++ gacha-info/src/pages/Home.js
@@ -4,12 +4,7 @@ import './Home.css';
 function Home() {
   return (
     <>
-    <div className="hero-banner-container">
-      <img src="/images/37_hero.png" alt=""></img>
-    </div>
-    
     <div>Home</div>
-    
     </>
    
   )
