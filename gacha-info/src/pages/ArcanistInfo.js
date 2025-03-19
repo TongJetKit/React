7b89807b0b36ea9691663ded69379f34840c92c4
TongJetKit <jetkittong@gmail.com>
2025-03-19 16:18:34 +0800

    Added loading time to ensure all data is obtain before showing it

--- gacha-info/src/pages/ArcanistInfo.js
+++ gacha-info/src/pages/ArcanistInfo.js
@@ -1,50 +1,78 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import "./ArcanistInfo.css";
 import Tab from "../components/Tab";
+import getSkill from '../database/Skill';
+import SkillCard from "../components/SkillCard";
 
 
 function ArcanistInfo() {
+  const [loading, setLoading] = useState(true);
+  const [skillInfo, setSkillInfo] = useState({});
   const { arcanistName } = useParams();
   const location = useLocation();
   const { arcanist } = location.state;
 
-  console.log(arcanist);
+  useEffect(() => {
+    getSkill(arcanistName).then((filtered_data) => {
+      setSkillInfo(filtered_data[0]);
+      setLoading(false);
+      // You can now access the filtered_data here
+    })
+      .catch((err) => {
+        console.log('Error fetching data:', err);
+      });
+  }, [])
 
   return (
-    <div className="arcanist-info">
-      <div className="hero-banner-container">
-        <img src="/images/37_hero.png" alt=""></img>
-      </div>
-      <div className="profile-container">
-        <div className="profile-info">
-          <div className="information-column">
-            <img className="avatar" src="/images/37.png" alt=""></img>
-            <div className="profile">
-              <div className="profile-name">
-                {`Name: ${arcanistName}`}
-              </div>
-              <div className="profile-tier">
-              {`Tier: ${arcanist.tier}`} <i class="fa-solid fa-star"></i>
-                </div>
-              <div className="profile-type">{`Type: ${arcanist.type}`}</div>
-              <div className={`profile-afflatus ${arcanist.afflatus}`}>
-              {`Afflatus: ${arcanist.afflatus}`}
-              </div>
-            </div>
+    <>
+      {(loading) ? (
+        <div className="loading-screen">
+          <span className="loading loading-spinner size-24"></span>
+        </div>
+      ) :
+        (<div className="arcanist-info">
+          <div className="hero-banner-container">
+            <img src="/images/37_hero.png" alt=""></img>
           </div>
-          {/* <div className="insight-column">
+          <div className="profile-container">
+            <div className="profile-info">
+              <div className="information-column">
+                <img className="avatar" src="/images/37.png" alt=""></img>
+                <div className="profile">
+                  <div className="profile-name">
+                    {`Name: ${arcanistName}`}
+                  </div>
+                  <div className="profile-tier">
+                    {`Tier: ${arcanist.tier}`} <i class="fa-solid fa-star"></i>
+                  </div>
+                  <div className="profile-type">{`Type: ${arcanist.type}`}</div>
+                  <div className={`profile-afflatus ${arcanist.afflatus}`}>
+                    {`Afflatus: ${arcanist.afflatus}`}
+                  </div>
+                </div>
+              </div>
+              {/* <div className="insight-column">
             <img src="/images/37_i2.png" alt=""></img>
           </div> */}
-        </div>
-         <div className="combat-info-tabs">
-           <Tab topics={["Skill", "Potray","Insight"]}></Tab>
+            </div>
+            <div className="combat-info-tabs">
+              <Tab topics={["Skill", "Potray", "Insight"]}>
+                <SkillCard skillInfo={skillInfo} afflatus={arcanist.afflatus}></SkillCard>
+                <SkillCard skillInfo={skillInfo}></SkillCard>
+              </Tab>
+            </div>
           </div>
-      </div>
-    </div>
+        </div>)
+
+      }
+
+
+
+    </>
   );
 
-  {
+  
     /* <div className="profile-introduction">
           <div className="tabs">
             <button
@@ -74,7 +102,7 @@ function ArcanistInfo() {
             </div>
           </div>
         </div> */
-  }
+  
 }
 
 export default ArcanistInfo;
