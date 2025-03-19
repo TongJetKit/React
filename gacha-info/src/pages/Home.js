7def51f5bc4c7515baeb958e5b39a24ffb5cf416
TongJetKit <jetkittong@gmail.com>
2025-03-19 16:10:15 +0800

    Changed the home page design

--- gacha-info/src/pages/Home.js
+++ gacha-info/src/pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className="homePage">
-      <img className="mainPageImage" src="/images/mainPageImage.png" alt=""></img>
+      <div className="title">Marcus's Suitcase</div>
+      <div class="flex gap-4">
+            <Link className="btn btn-neutral" to="/arcanist">Arcanist</Link>
+    
+            {/* <a class="btn btn-neutral">
+                See our blog
+                <i class="fa-solid fa-blog"></i>
+            </a> */}
+        </div>
     </div>
    
   )
