1b41ce7fed8b35f4def7c5275ef5cfcd99a615e2
TongJetKit <jetkittong@gmail.com>
2025-04-14 14:18:24 +0800

    Change arcane card size and add border image for the ultimate card

--- gacha-info/src/components/SkillCard.js
+++ gacha-info/src/components/SkillCard.js
@@ -4,17 +4,27 @@ import "./SkillCard.css";
 function SkillCard(props) {
   console.log(props.skillInfo);
   console.log(props.skillInfo.skills.map((skillCard) => {
-    return skillCard.description;
+    return skillCard;
   }));
   return (
     <>
-      {props.skillInfo.skills.map((skill) => {
+      {props.skillInfo.skills.map((skill, idx) => {
         return (
           <div className="skill-card">
             <div className={`skill-header ${props.afflatus}`}>{skill.name}</div>
             <div className="skill-body">
-              <div className="skill-image">
-                <img src="/images/30660131.png" alt=""></img>
+              <div className="skill-image-container">
+                <div className="card-img">
+                  {skill.type === "ultimate" ?
+                    <>
+                      <img className="card-ultimate" src={`/images/${props.skillInfo.characterName}_skill_${idx +1}.png`} alt=""></img>
+                      <img className="card-ultimate-background" src="/images/card-ultimate-background.png" alt=""></img>
+                    </>
+                    :
+                    <img className="card-skill" src={`/images/${props.skillInfo.characterName}_skill_${idx +1}.png`} alt=""></img>
+                  }
+                </div>
+                {/* <img className={skill.type === "ultimate"? "card-ultimate" :"card-skill"} src="/images/30660131.png" alt=""></img> */}
               </div>
               <div className="skill-info-container">
                 {Array.isArray(skill.description) ? (
