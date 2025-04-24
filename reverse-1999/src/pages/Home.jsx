93519a067b40eefea8911eb93af121c842b6fe2c
TongJetKit <jetkittong@gmail.com>
2025-04-24 11:03:33 +0800

    changed css className

--- reverse-1999/src/pages/Home.jsx
+++ reverse-1999/src/pages/Home.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div className="homePage">
-      <div className="title">Marcus's Suitcase</div>
+      <div className="home-title">Marcus's Suitcase</div>
       <div className="flex gap-4">
-            <Link className="btn btn-neutral" to="/arcanist">Arcanist</Link>
+            <Link className="btn btn-primary" to="/arcanist">Arcanist</Link>
         </div>
     </div>
 
