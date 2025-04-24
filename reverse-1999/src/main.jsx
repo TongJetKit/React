20f4110b1e10d30eba840d7099a99c0a605e95a5
TongJetKit <jetkittong@gmail.com>
2025-04-24 10:10:08 +0800

    added an error overlay for runtime error

--- reverse-1999/src/main.jsx
+++ reverse-1999/src/main.jsx
@@ -5,6 +5,17 @@ import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 
+const showErrorOverlay = err => {
+  const ErrorOverlay = customElements.get('vite-error-overlay');
+  if (!ErrorOverlay) return;
+  console.error('Runtime Error:', err);
+  const overlay = new ErrorOverlay(err);
+  document.body.appendChild(overlay);
+};
+
+window.addEventListener('error', showErrorOverlay);
+window.addEventListener('unhandledrejection', ({ reason }) => showErrorOverlay(reason));
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
